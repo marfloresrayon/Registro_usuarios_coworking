73c8d2fb07a7b96df2fff445ce145341bf2378dc
RaquelGlez <gonzalez.c.raquel@gmail.com>
2018-08-13 20:04:41 -0500

    Pantalla final con datos del visitante

--- src/js/findRegister.js
+++ src/js/findRegister.js
@@ -5,10 +5,14 @@ firebase.database().ref('visit')
     cardPass.innerHTML =
     `<div id="cardPass" class="card publication">
       <div  class="card-body">
-        <p>${newUser.val().name}</p>
-        <p>${newUser.val().companyVisit}</p>
-        <p>${newUser.val().date}</p>
-        <p>${newUser.val().time}</p>
+        <h4>Información del visitante</h4>
+        <p>Nombre: ${newUser.val().name}</p>
+        <p>Empresa de origen: ${newUser.val().company}</p>
+        <p>Empresa que visita: ${newUser.val().companyVisit}</p>
+        <p>Persona que visita: ${newUser.val().employee}</p>
+        <p>Fecha: ${newUser.val().date}</p>
+        <p>Hora: ${newUser.val().time}</p>
+        <p>Clave unica: ${newUser.val().keyVisit}</p>
       </div>
     </div>`;
   });
